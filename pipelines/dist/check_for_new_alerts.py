76ae5cafcdd7d2860aa3aa6cc1f5915221adc55d
Solomon Negusse <solomon.negusse@wri.org>
2025-06-20 11:33:50 +0200

    check latest alerts

--- pipelines/dist/check_for_new_alerts.py
+++ pipelines/dist/check_for_new_alerts.py
@@ -1,2 +1,33 @@
-def check_for_new_alerts():
-    pass
+import boto3
+import requests
+from botocore.exceptions import ClientError
+
+
+def get_latest_version(dataset):
+    url = f"https://data-api.globalforestwatch.org/dataset/{dataset}/latest"
+    resp = requests.get(url)
+    resp.raise_for_status()
+    data = resp.json()
+    if data["status"] != "success":
+        raise ValueError(f"No latest version set for dataset {dataset}")
+
+    return data["data"]["version"]
+
+
+def s3_object_exists(bucket_name, object_key):
+    try:
+        s3_client = boto3.client("s3")
+        s3_client.head_object(Bucket=bucket_name, Key=object_key)
+        return True
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            return False
+        else:
+            raise
+
+
+def check_zarr_exists(dataset, version):
+    object_key = f"{dataset}/{version}/zarr/{dataset}_{version}.zarr"
+    bucket = "gfw-data-lake"
+
+    return s3_object_exists(bucket, object_key)
