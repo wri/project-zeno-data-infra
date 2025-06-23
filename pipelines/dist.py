d0058d9e85a239e0d417f9de4b571146bee2ee27
Dan Scales <dan.scales@gmail.com>
2025-06-23 10:29:11 -0700

    PZB-179 Compute parquet for dist alerts by natural lands
    Compute the zarr of the dist alerts if needed. Create the cluster
    outside of the computation functions, using Solomon's create_cluster
    function. Used some of Solomon's functions in check_for_new_alerts.py
    that he hasn't checked in yet.

--- pipelines/dist.py
+++ pipelines/dist.py
@@ -10,17 +10,10 @@ import xarray as xr
 import fsspec
 import pandas as pd
 from flox.xarray import xarray_reduce
-import numpy as np
 from flox import ReindexArrayType, ReindexStrategy
-import fsspec
-import numpy as np
-import xarray as xr
-import fsspec
-import pandas as pd
-from flox.xarray import xarray_reduce
-import numpy as np
 import boto3
-
+from dist.check_for_new_alerts import get_latest_version, s3_object_exists
+from dist.gadm_dist_alerts_by_natural_lands import gadm_dist_alerts_by_natural_lands
 
 CONTEXTUAL_LAYERS = [None, "wri_natural_lands"] #, "ldacs", "umd_land_cover"]
 AOIs = ["gadm_administrative_boundaries"] #, "birdlife_key_biodiversity_areas", "wdpa_protected_areas", "landmark_indigenous_areas"]
@@ -30,38 +23,32 @@ def get_uri(feature):
     uri = '/'.join(['s3:/'] + raw)
     return uri
 
-@task
-def get_new_dist_version() -> str:
-    # latest = requests.get("https://data-api.globalforestwatch.org/dataset/umd_glad_dist_alerts/latest").json()
-    # # todo, check if we've already created this
-    # return latest["data"]["version"]
-    pass
-
-
-@task
-def create_zarr(dist_version: str) -> str:
-    zarr_uri = f"s3://gfw-data-lake/umd_glad_dist_alerts/{dist_version}/raster/epsg-4326/zarr/date_conf.zarr"
-    s3 = boto3.client('s3')
-    try:
-        s3.head_object(Bucket="gfw-data-lake", Key=f"umd_glad_dist_alerts/{dist_version}/raster/epsg-4326/zarr/date_conf.zarr/zarr.json")
-        return zarr_uri
-    except:
-        pass
-    
+#@task
+def create_cluster():
     cluster = coiled.Cluster(
         name="dist_alerts_zonal_stat_count",
         region="us-east-1",
-        n_workers=20,
+        n_workers=1,
         tags={"project": "dist_alerts_zonal_stat"},
         scheduler_vm_types="r7g.xlarge",
         worker_vm_types="r7g.2xlarge",
-        compute_purchase_option="spot_with_fallback"
+        compute_purchase_option="spot_with_fallback",
     )
+    cluster.adapt(minimum=1, maximum=50)
     client = cluster.get_client()
-    logging.getLogger("distributed.client").setLevel(logging.ERROR) 
+
+    return client
+
+#@task
+def create_zarr(dataset: str, dist_version: str) -> str:
+    zarr_uri = f"s3://gfw-data-lake/{dataset}/{dist_version}/raster/epsg-4326/zarr/date_conf.zarr"
+    type = 'date_conf'
+    if s3_object_exists('gfw-data-lake',
+                        f"{dataset}/{dist_version}/raster/epsg-4326/zarr/{type}.zarr/zarr.json"):
+        return zarr_uri
 
     dist_alerts_tiles = pd.read_json(
-        
+        f"s3://gfw-data-lake/{dataset}/{dist_version}/raster/epsg-4326/10/40000/default/gdal-geotiff/tiles.geojson"
     )
     dist_alerts_tile_uris = dist_alerts_tiles.features.apply(get_uri)
     dist_alerts = xr.open_mfdataset(
@@ -78,25 +65,10 @@ def create_zarr(dist_version: str) -> str:
     date_conf = xr.merge((alert_conf, alert_date))
     date_conf.to_zarr(zarr_uri, mode="w")
 
-    client.close()
-    cluster.close()
-
     return zarr_uri
 
-@task
+#@task
 def analyze_gadm_dist(dist_zarr_uri):
-    cluster = coiled.Cluster(
-        name="dist_alerts_zonal_stat_count",
-        region="us-east-1",
-        n_workers=50,
-        tags={"project": "dist_alerts_zonal_stat"},
-        scheduler_vm_types="r7g.xlarge",
-        worker_vm_types="r7g.2xlarge",
-        compute_purchase_option="spot_with_fallback"
-    )
-    client = cluster.get_client()
-    logging.getLogger("distributed.client").setLevel(logging.ERROR) 
-
     dist_alerts = xr.open_zarr(dist_zarr_uri)
     countries_from_clipped = xr.open_zarr(
         's3://gfw-data-lake/gadm_administrative_boundaries/v4.1.85/raster/epsg-4326/zarr/adm0_clipped_to_dist.zarr'
@@ -151,23 +123,28 @@ def analyze_gadm_dist(dist_zarr_uri):
     df = pd.DataFrame(coord_dict)
     df.to_parquet('dist_alerts_by_adm2_raw.parquet', index=False)
 
-    client.close()
-    cluster.close()
-
-@task
+#@task
 def run_validation_suite():
     pass
 
-@flow
+#@flow
 def main() -> list[str]:
-    dist_version = get_new_dist_version()
-    if not dist_version:
+    dataset = 'umd_glad_dist_alerts'
+    dist_version = get_latest_version(dataset)
+    if s3_object_exists("gfw-data-lake", f"{dataset}/{dist_version}/raster/epsg-4326/zarr/dist_alerts_by_natural_lands_adm2_raw.parquet"):
         return []
 
-    dist_zarr_uri = create_zarr(dist_version)
+    client = create_cluster()
+    logging.getLogger("distributed.client").setLevel(logging.ERROR)
+
+    # Create dist zarr, if not already created.
+    dist_zarr_uri = create_zarr(dataset, dist_version)
 
     #results = analyze_dist.map(itertools.product(AOIs, CONTEXTUAL_LAYERS), dist_zarr_uri)
-    result = analyze_gadm_dist(dist_zarr_uri)
+    #result = analyze_gadm_dist(dist_zarr_uri)
+    result = gadm_dist_alerts_by_natural_lands(dist_zarr_uri)
+
+    client.shutdown()
 
     return [result]
 
