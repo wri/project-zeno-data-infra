e231d95bc7fbb93e25aaa7cd78899024be064039
Solomon Negusse <solomon.negusse@wri.org>
2025-06-26 13:33:16 +0200

    tests and refactor zarr creation

--- pipelines/dist/create_zarr.py
+++ pipelines/dist/create_zarr.py
@@ -8,6 +8,15 @@ from .check_for_new_alerts import s3_object_exists
 data_lake_bucket = "gfw-data-lake"
 
 
+def decode_alert_data(band_data) -> xr.Dataset:
+    """Convert encoded alert date_conf data into separate confidence and alert date variables."""
+    alert_date = (band_data % 10000).astype(np.uint16)
+    alert_conf = (band_data // 10000).astype(np.uint8)
+    alert_conf.name = "confidence"
+    alert_date.name = "alert_date"
+    return xr.merge((alert_conf, alert_date))
+
+
 def create_zarr(version, tile_uris, overwrite=False) -> str:
     """create a full extent zarr file in s3."""
     key = f"umd_glad_dist_alerts/{version}/raster/epsg-4326/zarr/umd_glad_dist_alerts_{version}.zarr"
@@ -16,19 +25,14 @@ def create_zarr(version, tile_uris, overwrite=False) -> str:
     if s3_object_exists(data_lake_bucket, f"{key}/zarr.json") and not overwrite:
         return zarr_uri
 
-    dataset = xr.open_mfdataset(
+    dataset = xr.open_dataset(
         tile_uris,
         parallel=True,
         chunks={"x": 10000, "y": 10000},
     ).band_data
 
-    alert_date = (dataset % 10000).astype(np.uint16)
-    alert_conf = (dataset // 10000).astype(np.uint8)
-    alert_conf.name = "confidence"
-    alert_date.name = "alert_date"
-
-    date_conf = xr.merge((alert_conf, alert_date))
-    date_conf.to_zarr(zarr_uri, mode="w")
+    decoded_alert_data = decode_alert_data(dataset)
+    decoded_alert_data.to_zarr(zarr_uri, mode="w")
 
     return zarr_uri
 
