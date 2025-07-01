53646576c00a90b708aaf06844adbbac89d71661
Gary Tempus Jr <gary.tempus@wri.org>
2025-07-01 16:51:57 -0400

    refactor(GADM Dist Alerts): move fixtures and do some typing

--- pipelines/dist/gadm_dist_alerts.py
+++ pipelines/dist/gadm_dist_alerts.py
@@ -1,12 +1,10 @@
 import logging
 from typing import Callable, Tuple
-
 import numpy as np
 import pandas as pd
 import xarray as xr
 from flox import ReindexArrayType, ReindexStrategy
 from flox.xarray import xarray_reduce
-
 
 DATA_LAKE_BUCKET = "gfw-data-lake"
 
@@ -28,7 +26,7 @@ def _default_loader(dist_zarr_uri: str) -> Tuple[xr.Dataset, xr.Dataset, xr.Data
     )
 
 
-def _default_saver(alerts_count_df, results_uri):
+def _default_saver(alerts_count_df: pd.DataFrame, results_uri: str) -> None:
     alerts_count_df.to_parquet(results_uri, index=False)
 
 
@@ -38,7 +36,7 @@ def gadm_dist_alerts(
         loader: LoaderType = _default_loader,
         groups: ExpectedGroupsType = None,
         saver: SaverType = _default_saver,
-):
+) -> str:
     """Count DIST alerts by GADM boundary, confidence, and date, and export grouped results to a Parquet file in S3."""
     logging.getLogger("distributed.client").setLevel(logging.ERROR)
     dist_alerts, country, region, subregion = loader(dist_zarr_uri)
@@ -54,17 +52,14 @@ def _processor(
         region: xr.Dataset,
         subregion: xr.Dataset,
         expected_groups: Tuple,
-):
-    if expected_groups:
-        groups = expected_groups
-    else:
-        groups = (
-            np.arange(894),
-            np.arange(86),
-            np.arange(854),
-            np.arange(731, 1590),
-            [1, 2, 3],
-        )
+) -> Tuple:
+    groups = (
+        np.arange(894),
+        np.arange(86),
+        np.arange(854),
+        np.arange(731, 1590),
+        [1, 2, 3],
+    ) if expected_groups is None else expected_groups
 
     return (
         dist_alerts.confidence,
@@ -79,7 +74,7 @@ def _processor(
     )
 
 
-def _compute(reduce_mask, reduce_groupbys, expected_groups):
+def _compute(reduce_mask: xr.DataArray, reduce_groupbys: Tuple, expected_groups: Tuple):
     return xarray_reduce(
         reduce_mask,
         *reduce_groupbys,
@@ -92,23 +87,22 @@ def _compute(reduce_mask, reduce_groupbys, expected_groups):
     ).compute()
 
 
-def _create_data_frame(alerts_count):
+def _create_data_frame(alerts_count: xr.Dataset) -> pd.DataFrame:
     sparse_data = alerts_count.data
-
     dim_names = alerts_count.dims
     indices = sparse_data.coords
     values = sparse_data.data
 
     coord_dict = {
-        dim: alerts_count.coords[dim].values[indices[i]]
-        for i, dim in enumerate(dim_names)
+        dim: alerts_count.coords[dim].values[idx]
+        for dim, idx in zip(dim_names, indices)
     }
     coord_dict["value"] = values
 
     return pd.DataFrame(coord_dict)
 
 
-def _save_results(alerts_count_df, dist_version, saver):
+def _save_results(alerts_count_df: pd.DataFrame, dist_version: str, saver: Callable) -> str:
     results_uri = f"s3://{DATA_LAKE_BUCKET}/umd_glad_dist_alerts/{dist_version}/tabular/epsg-4326/zonal_stats/dist_alerts_by_adm2_raw_test.parquet"
     saver(alerts_count_df, results_uri)
     return results_uri
