0933db64f8c8d5feba5aaf4b05cff92a14b3fdcd
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-07-01 14:22:07 -0400

    Remove explicit version pin for distributed, which is pulled in/dictated by dask[distributed]

--- pipelines/dist_flow.py
+++ pipelines/dist_flow.py
@@ -35,10 +35,10 @@ def get_tiles(dist_version):
 @task
 def create_cluster():
     cluster = coiled.Cluster(
-        name="dist_alerts_zonal_stat_count_multi",
+        name="dist_alerts_zonal_stat_count",
         region="us-east-1",
         n_workers=10,
-        container="globalforestwatch/zeno:1",
+        container="globalforestwatch/zeno:2",
         tags={"project": "dist_alerts_zonal_stat"},
         scheduler_vm_types="r7g.xlarge",
         worker_vm_types="r7g.2xlarge",
