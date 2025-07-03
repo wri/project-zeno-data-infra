4492b41b2969806bb5f603dde00fdec604b1e7f6
Justin Terry <justin.terry@wri.org>
2025-07-03 10:40:49 -0700

    Use pd dataframes to assert equality

--- test/integration/api/test_dist_analytics.py
+++ test/integration/api/test_dist_analytics.py
@@ -1,46 +1,62 @@
+import pandas as pd
 from fastapi.testclient import TestClient
-from api.app.main import get_analytics_result, DistAlertsAnalyticsIn, AdminAreaOfInterest, app
-import pytest
 
+from api.app.main import app
 
 client = TestClient(app)
 
 
 def test_gadm_dist_analytics_no_intersection():
     resource = client.post(
-        "/v0/land_change/dist_alerts/analytics", 
+        "/v0/land_change/dist_alerts/analytics",
         json={
             "aois": [{"type": "admin", "id": "IDN.24.9"}],
             "start_date": "2024-08-15",
             "end_date": "2024-08-16",
             "intersections": [],
-        }
+        },
     ).json()
-        
+
     resource_id = resource["data"]["link"].split("/")[-1]
 
-    data = client.get(f"/v0/land_change/dist_alerts/analytics/{resource_id}").json()["data"]
-    assert data["result"]["country"] == ["IDN", "IDN"]
-    assert data["result"]["region"] == [24, 24]
-    assert data["result"]["subregion"] == [9, 9]
-    assert data["result"]["alert_date"] == ["2024-08-15T00:00:00", "2024-08-15T00:00:00"]
-    assert data["result"]["confidence"] == ["high", "low"]
+    data = client.get(f"/v0/land_change/dist_alerts/analytics/{resource_id}").json()[
+        "data"
+    ]
+
+    expected_df = pd.DataFrame(
+        {
+            "country": ["IDN", "IDN"],
+            "region": [24, 24],
+            "subregion": [9, 9],
+            "alert_date": [
+                "2024-08-15T00:00:00",
+                "2024-08-15T00:00:00",
+            ],  # TODO use YYYY-MM-DD
+            "confidence": ["high", "low"],
+            "value": [1490.0, 95.0],  # TODO use int
+        }
+    )
+
+    actual_df = pd.DataFrame(data["result"])
+    pd.testing.assert_frame_equal(expected_df, actual_df)
 
 
 def test_kba_dist_analytics_no_intersection():
     resource = client.post(
-        "/v0/land_change/dist_alerts/analytics", 
+        "/v0/land_change/dist_alerts/analytics",
         json={
             "aois": [{"type": "key_biodiversity_area", "id": "8111"}],
             "start_date": "2024-08-15",
             "end_date": "2024-08-16",
             "intersections": [],
-        }
+        },
     ).json()
-        
+
     resource_id = resource["data"]["link"].split("/")[-1]
 
-    data = client.get(f"/v0/land_change/dist_alerts/analytics/{resource_id}").json()["data"]
+    data = client.get(f"/v0/land_change/dist_alerts/analytics/{resource_id}").json()[
+        "data"
+    ]
     print(data)
     assert data["result"]["key_biodiversity_area"] == ["8111"]
     assert data["result"]["alert_date"] == ["2024-08-15"]
