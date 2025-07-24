c549b8224543c548ec865abee84f4ff95c90ef95
Gary Tempus Jr <gary.tempus@wri.org>
2025-07-24 16:33:28 -0400

    feat(TCL): ensure that years are >= 2001

--- api/app/models/land_change/tree_cover_loss.py
+++ api/app/models/land_change/tree_cover_loss.py
@@ -1,5 +1,5 @@
 from typing import Union, List, Annotated, Literal
-from pydantic import Field
+from pydantic import Field, field_validator
 
 from app.models.common.base import StrictBaseModel
 from app.models.common.areas_of_interest import (
@@ -23,14 +23,14 @@ class TreeCoverLossAnalyticsIn(StrictBaseModel):
     start_year: str = Field(
         ...,
         title="Start Date",
-        description="Must be either year or YYYY-MM-DD date format.",
+        description="Must be year in YYYY date format. Minimum year is 2001.",
         pattern=DATE_REGEX,
         examples=["2020", "2024"],
     )
     end_year: str = Field(
         ...,
         title="End Date",
-        description="Must be either year or YYYY-MM-DD date format.",
+        description="Must be year in YYYY date format. Minimum year is 2001.",
         pattern=DATE_REGEX,
         examples=["2023", "2024"],
     )
@@ -41,3 +41,11 @@ class TreeCoverLossAnalyticsIn(StrictBaseModel):
     intersections: List[Literal["driver", "natural_lands"]] = Field(
         ..., min_length=0, max_length=1, description="List of intersection types"
     )
+
+    # Validator to ensure year is >= 2001
+    @field_validator('start_year', 'end_year')
+    def year_must_be_at_least_2001(cls, v: str) -> str:
+        year_int = int(v)
+        if year_int < 2001:
+            raise ValueError('Year must be at least 2001')
+        return v
