0b0e5de6edce874733e091b2b8610b60791ecb09
Solomon Negusse <solomon.negusse@wri.org>
2025-08-01 15:44:39 +0300

    add test for invalid input, cleanup

--- api/test/unit/routers/land_change/grasslands/test_grasslands.py
+++ api/test/unit/routers/land_change/grasslands/test_grasslands.py
@@ -46,24 +46,13 @@ class TestGrasslandsAnalyticsPostWithAdmin:
         print("result", resource)
         assert (
             resource["data"]["link"]
-            == "http://testserver/v0/land_change/grasslands/analytics/91d617f8-4d15-5b1c-9b49-3b742935356c"
+            == "http://testserver/v0/land_change/grasslands/analytics/f1f3ddee-61c1-52aa-9c6a-ec16b3f14ac7"
         )
 
     @pytest.mark.asyncio
     async def test_post_returns_202_accepted_response_code(self, test_request):
         response = test_request
         assert response.status_code == 202
-
-
-import pytest
-from unittest.mock import patch
-import pytest_asyncio
-from app.main import app
-from asgi_lifespan import LifespanManager
-from fastapi.testclient import TestClient
-from httpx import ASGITransport, AsyncClient
-
-client = TestClient(app)
 
 
 class TestGrasslandsAnalyticsPostWithCustomAOI:
@@ -95,7 +84,9 @@ class TestGrasslandsAnalyticsPostWithCustomAOI:
                     test_request = await client.post(
                         "/v0/land_change/grasslands/analytics",
                         json={
-                            "aoi": aoi
+                            "aoi": aoi,
+                            "start_year": "2003",
+                            "end_year": "2023",
                         },
                     )
 
@@ -112,7 +103,7 @@ class TestGrasslandsAnalyticsPostWithCustomAOI:
         resource = test_request.json()
         assert (
             resource["data"]["link"]
-            == "http://testserver/v0/land_change/grasslands/analytics/9067221f-fc65-56c8-a4c8-6af182b03240"
+            == "http://testserver/v0/land_change/grasslands/analytics/51a11cba-5d15-597d-864c-5b69f020cadd"
         )
 
     @pytest.mark.asyncio
@@ -120,3 +111,33 @@ class TestGrasslandsAnalyticsPostWithCustomAOI:
         response = test_request
         assert response.status_code == 202
 
+
+class TestGrasslandsAnalyticsPostWithInvalidInput:
+    @pytest_asyncio.fixture(autouse=True)
+    async def test_request(self):
+        """Runs before each test in this class"""
+
+        with patch("app.routers.land_change.grasslands.grasslands.Path.exists", return_value=False) as mock_exists, \
+            patch("app.routers.land_change.grasslands.grasslands.Path.mkdir") as mock_mkdir, \
+            patch("app.routers.land_change.grasslands.grasslands.Path.write_text") as mock_write_text, \
+            patch("app.routers.land_change.grasslands.grasslands.do_analytics") as mock_add_task:
+            async with LifespanManager(app):
+                async with AsyncClient(
+                    transport=ASGITransport(app), base_url="http://testserver"
+            ) as client:
+                    test_request = await client.post(
+                        "/v0/land_change/grasslands/analytics",
+                        json={
+                            "aoi": {"type": "admin", "ids": "IDN.24.9"},
+                            "start_year": "2000",
+                            "end_year": "2023",
+                        },
+                    )
+
+                    yield test_request
+
+
+    @pytest.mark.asyncio
+    async def test_post_with_invalid_input_returns_422(self, test_request):
+        response = test_request
+        assert response.status_code == 422
