c5ccced21e83e464c6c717adc25e6336d282c63c
Dan Scales <dan.scales@gmail.com>
2025-08-01 11:18:39 -0700

    Move the examples to NaturalLandsAnalyticsResponse
    Responding to Gary's comment.

--- api/app/models/land_change/natural_lands.py
+++ api/app/models/land_change/natural_lands.py
@@ -30,18 +30,7 @@ class NaturalLandsAnalyticsIn(StrictBaseModel):
 
 
 class NaturalLandsAnalytics(StrictBaseModel):
-    result: Optional[dict] = {  # column oriented for loading into a dataframe
-        "__dtypes__": {
-            "country": "str",
-            "region": "int",
-            "subregion": "int",
-            "natural_lands_area": "float32",
-        },
-        "country": ["BRA", "BRA", "BRA"],
-        "region": [1, 1, 1],
-        "subregion": [12, 12, 12],
-        "natural_lands_area": [4851452000, 4084129000, 253008000],
-    }
+    result: Optional[dict] = None
     metadata: Optional[dict] = None
     message: Optional[str] = None
     status: AnalysisStatus
@@ -54,3 +43,33 @@ class NaturalLandsAnalytics(StrictBaseModel):
 
 class NaturalLandsAnalyticsResponse(Response):
     data: NaturalLandsAnalytics
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "data": {
+                        "result": {  # column oriented for loading into a dataframe
+                            "__dtypes__": {
+                                "country": "str",
+                                "region": "int",
+                                "subregion": "int",
+                                "natural_lands_area": "float32",
+                            },
+                            "country": ["BRA", "BRA", "BRA"],
+                            "region": [1, 1, 1],
+                            "subregion": [12, 12, 12],
+                            "natural_lands_area": [4851452000, 4084129000, 253008000],
+                        },
+                        "metadata": {
+                            "aoi": {
+                                "type": "admin",
+                                "ids": ["BRA.1.12"],
+                            },
+                        },
+                        "message": "",
+                        "status": "saved",
+                    }
+                }
+            ]
+        }
+    }
