bd4201cef676d92ee70f44b73d8a1b90eedecf6f
Justin Terry <justin.terry@wri.org>
2025-08-04 11:01:18 -0700

    Move analysis out to class

--- api/app/use_cases/analysis/tree_cover/tree_cover_service.py
+++ api/app/use_cases/analysis/tree_cover/tree_cover_service.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from app.models.land_change.tree_cover import TreeCoverAnalyticsIn
 
 
@@ -5,46 +6,88 @@ class TreeCoverService:
     def __init__(self, utils=None):
         pass
 
-    def do(self, tree_cover_analytics: TreeCoverAnalyticsIn):
+    async def do(self, tree_cover_analytics: TreeCoverAnalyticsIn):
         pass
 
-    def compute(self, tree_cover_analytics: TreeCoverAnalyticsIn):
+    async def compute(self, tree_cover_analytics: TreeCoverAnalyticsIn):
         """
         Placeholder for the actual computation logic.
         This method should implement the logic to compute tree cover analytics.
         """
+        analyzer = get_tree_cover_analyzer(tree_cover_analytics)
+        return await analyzer.compute()
+
+
+def get_tree_cover_analyzer(tree_cover_analytics: TreeCoverAnalyticsIn):
+    if tree_cover_analytics.aoi.type == "admin":
+        return GadmTreeCoverAnalyzer(
+            tree_cover_analytics.aoi.type, tree_cover_analytics.aoi.ids
+        )
+
+
+class GadmTreeCoverAnalyzer:
+    def __init__(self, tree_cover_analytics_in: TreeCoverAnalyticsIn):
+        self.tree_cover_analytics_in = tree_cover_analytics_in
+
+    async def compute(self) -> dict:
+        iso_results = self.query_for_results(admin_level)
+        adm1_results = self.query_for_results(admin_level)
+        adm2_results = self.query_for_results(admin_level)
+
+        combined_results = pd.concat([iso_results, adm1_results, adm2_results])
+        return combined_results.dict(orient="list")
+
+    async def query_for_results(admin_level):
+        # get query for admin level
+        # send request to data api
+        # convert to create new results
         pass
 
-    def create_query_from_analytics(self, tree_cover_analytics: TreeCoverAnalyticsIn):
+    def merge_back_gadm_ids(
+        self, admin_level: int, results: pd.DataFrame
+    ) -> pd.DataFrame:
+        # Join 'iso', 'adm1', and 'adm2' columns into a new 'id' column separated by '.'
+        join_cols = ["iso"]
+        if admin_level >= 1:
+            join_cols.append("adm1")
+        elif admin_level >= 2:
+            join_cols.append("adm2")
+
+        results["id"] = results[join_cols].astype(str).agg(".".join, axis=1)
+
+        # Drop the original 'iso', 'adm1', and 'adm2' columns if they exist
+        results = results.drop(columns=join_cols)
+        return results
+
+    def create_query_from_analytics(self) -> tuple[str, str, str]:
         """
         Creates a Query object based on the fields of TreeCoverAnalyticsIn.
         """
 
-        if tree_cover_analytics.aoi.type == "admin":
-            iso_query = self.build_query_for_admin_level(
-                tree_cover_analytics.aoi.ids,
-                0,
-                tree_cover_analytics.canopy_cover,
-                tree_cover_analytics.forest_filter,
-            )
+        iso_query = self.build_sql_for_admin_level(
+            self.tree_cover_analytics_in.aoi.ids,
+            0,
+            self.tree_cover_analytics_in.canopy_cover,
+            self.tree_cover_analytics_in.forest_filter,
+        )
 
-            adm1_query = self.build_query_for_admin_level(
-                tree_cover_analytics.aoi.ids,
-                1,
-                tree_cover_analytics.canopy_cover,
-                tree_cover_analytics.forest_filter,
-            )
+        adm1_query = self.build_sql_for_admin_level(
+            self.tree_cover_analytics_in.aoi.ids,
+            1,
+            self.tree_cover_analytics_in.canopy_cover,
+            self.tree_cover_analytics_in.forest_filter,
+        )
 
-            adm2_query = self.build_query_for_admin_level(
-                tree_cover_analytics.aoi.ids,
-                2,
-                tree_cover_analytics.canopy_cover,
-                tree_cover_analytics.forest_filter,
-            )
+        adm2_query = self.build_sql_for_admin_level(
+            self.tree_cover_analytics_in.aoi.ids,
+            2,
+            self.tree_cover_analytics_in.canopy_cover,
+            self.tree_cover_analytics_in.forest_filter,
+        )
 
-            return (iso_query, adm1_query, adm2_query)
+        return (iso_query, adm1_query, adm2_query)
 
-    def build_query_for_admin_level(
+    def build_sql_for_admin_level(
         self, ids, admin_level, canopy_cover, forest_filter=None
     ):
         admin_level_ids = []
--- api/test/unit/use_cases/test_tree_cover_service.py
+++ api/test/unit/use_cases/test_tree_cover_service.py
@@ -1,5 +1,8 @@
 from app.models.land_change.tree_cover import TreeCoverAnalyticsIn
-from app.use_cases.analysis.tree_cover.tree_cover_service import TreeCoverService
+from app.use_cases.analysis.tree_cover.tree_cover_service import (
+    GadmTreeCoverAnalyzer,
+    TreeCoverService,
+)
 
 
 def test_create_query_from_analytics_iso_only():
@@ -12,8 +15,8 @@ def test_create_query_from_analytics_iso_only():
         forest_filter="primary_forest",
     )
 
-    service = TreeCoverService()
-    query = service.create_query_from_analytics(analytics_in)
+    analyzer = GadmTreeCoverAnalyzer(analytics_in)
+    query = analyzer.create_query_from_analytics()
     assert (
         query[0]
         == "SELECT iso, SUM(umd_tree_cover_extent_2000__ha) AS umd_tree_cover_extent_2000__ha FROM data WHERE umd_tree_cover_density_2000__threshold = 30 AND iso in ('IDN', 'BRA', 'COD') AND is__umd_regional_primary_forest_2001 = true GROUP BY iso"
@@ -30,8 +33,8 @@ def test_create_query_from_analytics_adm1_only():
         forest_filter="primary_forest",
     )
 
-    service = TreeCoverService()
-    query = service.create_query_from_analytics(analytics_in)
+    analyzer = GadmTreeCoverAnalyzer(analytics_in)
+    query = analyzer.create_query_from_analytics()
     assert (
         query[1]
         == "SELECT iso, adm1, SUM(umd_tree_cover_extent_2000__ha) AS umd_tree_cover_extent_2000__ha FROM data WHERE umd_tree_cover_density_2000__threshold = 30 AND (iso, adm1) in (('IDN', 12), ('BRA', 1), ('COD', 5)) AND is__umd_regional_primary_forest_2001 = true GROUP BY iso, adm1"
@@ -48,8 +51,8 @@ def test_create_query_from_analytics_adm2_only():
         forest_filter="primary_forest",
     )
 
-    service = TreeCoverService()
-    query = service.create_query_from_analytics(analytics_in)
+    analyzer = GadmTreeCoverAnalyzer(analytics_in)
+    query = analyzer.create_query_from_analytics()
     assert (
         query[2]
         == "SELECT iso, adm1, adm2, SUM(umd_tree_cover_extent_2000__ha) AS umd_tree_cover_extent_2000__ha FROM data WHERE umd_tree_cover_density_2000__threshold = 30 AND (iso, adm1, adm2) in (('IDN', 12, 9), ('BRA', 1, 12), ('COD', 5, 15)) AND is__umd_regional_primary_forest_2001 = true GROUP BY iso, adm1, adm2"
