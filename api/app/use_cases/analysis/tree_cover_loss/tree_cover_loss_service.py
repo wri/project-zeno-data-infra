a8e900fb69554e3d82ab7aff246ae32acdf9b4b7
Gary Tempus Jr <gary.tempus@wri.org>
2025-08-05 16:27:40 -0400

    refactor (TCL Router and Service): depend more on service

--- api/app/use_cases/analysis/tree_cover_loss/tree_cover_loss_service.py
+++ api/app/use_cases/analysis/tree_cover_loss/tree_cover_loss_service.py
@@ -1,13 +1,22 @@
+from uuid import UUID
+
 from app.models.land_change.tree_cover_loss import TreeCoverLossAnalyticsIn
 from app.models.common.analysis import AnalysisStatus
 
 
 class TreeCoverLossService:
-    def __init__(self, utils=None):
+    def __init__(self):
+        self.analytics_resource = None
+
+    def do(self) -> None:
         pass
 
-    def do(self, tree_cover_loss_analytics: TreeCoverLossAnalyticsIn):
-        pass
-
-    def get_status(self):
+    def get_status(self) -> AnalysisStatus:
         return AnalysisStatus.pending
+
+    def set_resource_from(self, data: TreeCoverLossAnalyticsIn):
+        self.analytics_resource = data
+
+    def resource_thumbprint(self) -> UUID:
+        return self.analytics_resource.thumbprint()
+    
