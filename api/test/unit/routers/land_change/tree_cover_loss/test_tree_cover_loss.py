604b62b4b8c69525ddb9f258bd3e02ce67b27937
Gary Tempus <gtempus@users.noreply.github.com>
2025-08-06 19:08:24 -0400

    PZB-260: Create Factory and Analyzer Abstractions (#54)
    I imagine the analyzer and repository being created with help from FastAPI Depends. This pushes all of the implementation-specific items up to the edge of the app and away from the domain objects:
    
    def get_analysis_repository() -> AnalysisRepository:  # Use your actual base type
        return FileSystemAnalysisRepository(resource_directory="tree_cover_loss_analytics_payloads")
    
    def get_dataset_repository():
        pass
        # return AwsS3DatasetRepository()
    
    @router.post(
        "/analytics",
        response_class=ORJSONResponse,
        response_model=DataMartResourceLinkResponse,
        status_code=202,
    )
    async def create(
        *,
        data: TreeCoverLossAnalyticsIn,
        request: Request,
        background_tasks: BackgroundTasks,
        analysis_repository_implementation: AnalysisRepository = Depends(get_analysis_repository),
        dataset_repository_implementation = Depends(get_dataset_repository),  # DatasetRepository
    ):
        try:
            service = TreeCoverLossService(
                analysis_repository= TreeCoverLossAnalysisRepository(analysis_repository_implementation),
                analyzer=TreeCoverLossAnalyzer(
                    analysis_repository=TreeCoverLossAnalysisRepository(analysis_repository_implementation),
                    otf_compute_engine=request.app.state.dask_client,
                    dataset_repository=TreeCoverLossDatasetRepository(dataset_repository_implementation)
                )
            )
            await service.set_resource_from(data)
            background_tasks.add_task(service.do)
            return _datamart_resource_link_response(request, service)
        except Exception as e:
            logging.error(
                {
                    "event": "tree_cover_loss_analytics_processing_failure",
                    "severity": "high",  # Helps with alerting
                    "error_type": e.__class__.__name__,  # e.g., "ValueError", "ConnectionError"
                    "error_details": str(e),
                    "stack_trace": traceback.format_exc(),
                }
            )
            raise HTTPException(status_code=500, detail="Internal Server Error")

--- api/test/unit/routers/land_change/tree_cover_loss/test_tree_cover_loss.py
+++ api/test/unit/routers/land_change/tree_cover_loss/test_tree_cover_loss.py
@@ -1,64 +1,62 @@
+import json
 from unittest.mock import patch
 
 import pytest
 from app.main import app
-from app.routers.land_change.tree_cover_loss.tree_cover_loss import (
-    TreeCoverLossAnalyticsIn,
-)
-from app.use_cases.analysis.tree_cover_loss.tree_cover_loss_service import (
-    TreeCoverLossService,
-)
+from app.routers.land_change.tree_cover_loss.tree_cover_loss import TreeCoverLossAnalyticsIn
+from app.use_cases.analysis.tree_cover_loss.tree_cover_loss_service import TreeCoverLossService
 from fastapi.testclient import TestClient
+from app.models.common.areas_of_interest import AdminAreaOfInterest
+from app.models.common.base import DataMartResourceLinkResponse, DataMartResourceLink
+from app.models.common.analysis import AnalysisStatus
 
 client = TestClient(app)
 
+@pytest.fixture
+def dummy_analytics_in():
+    return TreeCoverLossAnalyticsIn(
+        aoi=AdminAreaOfInterest(
+            type= "admin",
+            ids= ["IDN.24.9"],
+        ),
+        start_year="2023",
+        end_year="2024",
+        canopy_cover=30,
+        forest_filter="primary_forest",
+        intersections=["driver"],
+    )
+
 
 class TestTreeCoverLossPostUseCaseInitiation:
-    @patch(
-        "app.use_cases.analysis.tree_cover_loss.tree_cover_loss_service.TreeCoverLossService.do"
-    )
-    def test_dates_happy_path_start_year_is_before_end_year(
-        self, tree_cover_loss_service: TreeCoverLossService
-    ):
-        tree_cover_loss_analytics_in = TreeCoverLossAnalyticsIn(
-            aoi={
-                "type": "admin",
-                "ids": ["IDN.24.9"],
-            },
-            start_year="2023",
-            end_year="2024",
-            canopy_cover=30,
-            forest_filter="primary_forest",
-            intersections=["driver"],
-        )
-        client.post(
-            "/v0/land_change/tree_cover_loss/analytics",
-            json={
-                "aoi": {"type": "admin", "ids": ["IDN.24.9"]},
-                "start_year": "2023",
-                "end_year": "2024",
-                "canopy_cover": 30,
-                "forest_filter": "primary_forest",
-                "intersections": ["driver"],
-            },
-        )
+    def test_router_sets_the_resource_in_the_service(self, dummy_analytics_in):
+        with patch.object(TreeCoverLossService, 'set_resource_from') as mock_set_resource, \
+            patch.object(TreeCoverLossService, 'do'):
 
-        tree_cover_loss_service.assert_called_with(tree_cover_loss_analytics_in)
+            client.post(
+                "/v0/land_change/tree_cover_loss/analytics",
+                json=json.loads(dummy_analytics_in.model_dump_json()),
+            )
 
-    @pytest.mark.xfail
-    def test_post_returns_pending_status(self):
-        resource = self.test_request.json()
-        assert resource["status"] == "pending"
+            mock_set_resource.assert_called_with(dummy_analytics_in)
 
-    @pytest.mark.xfail
-    def test_post_returns_resource_link(self):
-        resource = self.test_request.json()
-        assert (
-            resource["data"]["link"]
-            == "http://testserver/v0/land_change/tree_cover_loss/analytics/9c4b9bb5-0ecd-580d-85c9-d9a112a69b59"
-        )
+    def test_router_returns_DataMartResourceLinkResponse(self, dummy_analytics_in):
+        with patch.object(TreeCoverLossService, 'do'):
+            response = client.post(
+                "/v0/land_change/tree_cover_loss/analytics",
+                json=json.loads(dummy_analytics_in.model_dump_json()),
+            )
 
-    @pytest.mark.xfail
-    def test_post_202_accepted_response_code(self):
-        response = self.test_request
-        assert response.status_code == 202
+            assert response.json() == json.loads(DataMartResourceLinkResponse(
+                data=DataMartResourceLink(link="http://testserver/v0/land_change/tree_cover_loss/analytics/12665e7b-e976-5ab2-adc2-c4576399f0bb"),
+                status=AnalysisStatus.pending
+            ).model_dump_json())
+
+
+    def test_post_202_accepted_response_code(self, dummy_analytics_in):
+        with patch.object(TreeCoverLossService, 'do'):
+            response = client.post(
+                "/v0/land_change/tree_cover_loss/analytics",
+                json=json.loads(dummy_analytics_in.model_dump_json()),
+            )
+
+            assert response.status_code == 202
