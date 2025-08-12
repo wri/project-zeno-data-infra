394409a3c559b9c87f23e8c7dedf163845752a07
Gary Tempus Jr <gary.tempus@wri.org>
2025-08-12 09:56:17 -0400

    refactor: use general analytics router flow in `GET`

--- api/app/routers/common_analytics.py
+++ api/app/routers/common_analytics.py
@@ -1,11 +1,19 @@
 import logging
 import traceback
 from typing import Callable
+from uuid import UUID
 
-from app.models.common.analysis import AnalyticsIn
+from app.domain.models.analysis import Analysis
+from app.domain.repositories.analysis_repository import AnalysisRepository
+from app.models.common.analysis import AnalysisStatus, AnalyticsIn
 from app.models.common.base import DataMartResourceLink, DataMartResourceLinkResponse
+from app.models.land_change.tree_cover_loss import (
+    TreeCoverLossAnalytics,
+    TreeCoverLossAnalyticsResponse,
+)
 from app.use_cases.analysis.analysis_service import AnalysisService
 from fastapi import BackgroundTasks, HTTPException, Request
+from fastapi import Response as FastAPIResponse
 
 
 async def create_analysis(
@@ -41,3 +49,51 @@ async def create_analysis(
             }
         )
         raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+async def get_analysis(
+    resource_id: UUID,
+    analysis_repository: AnalysisRepository,
+    response: FastAPIResponse,
+):
+    analysis: Analysis = Analysis(result=None, metadata=None, status=None)
+
+    try:
+        analysis = await analysis_repository.load_analysis(resource_id)
+    except Exception as e:
+        logging.error(
+            {
+                "event": "tree_cover_loss_analytics_resource_request_failure",
+                "severity": "high",
+                "resource_id": resource_id,
+                "resource_metadata": analysis.metadata,
+                "error_type": e.__class__.__name__,
+                "error_details": str(e),
+                "traceback": traceback.format_exc(),
+            }
+        )
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+    if analysis.status is None:
+        raise HTTPException(status_code=404, detail="Analysis not found")
+
+    match analysis.status:
+        case AnalysisStatus.pending:
+            response.headers["Retry-After"] = "1"
+            message = "Resource is still processing, follow Retry-After header."
+        case AnalysisStatus.saved:
+            message = "Analysis completed successfully."
+        case AnalysisStatus.failed:
+            message = "Analysis failed. Result is not available."
+        case _:
+            message = ""  # Optional default
+
+    return TreeCoverLossAnalyticsResponse(
+        data=TreeCoverLossAnalytics(
+            status=analysis.status,
+            message=message,
+            result=analysis.result,
+            metadata=analysis.metadata,
+        ),
+        status="success",
+    )
--- api/app/routers/land_change/tree_cover_loss/tree_cover_loss.py
+++ api/app/routers/land_change/tree_cover_loss/tree_cover_loss.py
@@ -1,9 +1,6 @@
-import logging
 import os
-import traceback
 
 from app.domain.analyzers.tree_cover_loss_analyzer import TreeCoverLossAnalyzer
-from app.domain.models.analysis import Analysis
 from app.domain.repositories.analysis_repository import AnalysisRepository
 from app.infrastructure.external_services.data_api_compute_service import (
     DataApiComputeService,
@@ -11,16 +8,14 @@ from app.infrastructure.external_services.data_api_compute_service import (
 from app.infrastructure.persistence.file_system_analysis_repository import (
     FileSystemAnalysisRepository,
 )
-from app.models.common.analysis import AnalysisStatus
 from app.models.common.base import DataMartResourceLinkResponse
 from app.models.land_change.tree_cover_loss import (
-    TreeCoverLossAnalytics,
     TreeCoverLossAnalyticsIn,
     TreeCoverLossAnalyticsResponse,
 )
-from app.routers.common_analytics import create_analysis
+from app.routers.common_analytics import create_analysis, get_analysis
 from app.use_cases.analysis.analysis_service import AnalysisService
-from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, Request
+from fastapi import APIRouter, BackgroundTasks, Depends, Request
 from fastapi import Response as FastAPIResponse
 from fastapi.responses import ORJSONResponse
 from pydantic import UUID5
@@ -86,51 +81,8 @@ async def get_tcl_analytics_result(
     response: FastAPIResponse,
     analysis_repository: AnalysisRepository = Depends(get_analysis_repository),
 ):
-    analysis: Analysis = Analysis(result=None, metadata=None, status=None)
-
-    try:
-        analysis = await analysis_repository.load_analysis(resource_id)
-    except Exception as e:
-        logging.error(
-            {
-                "event": "tree_cover_loss_analytics_resource_request_failure",
-                "severity": "high",  # Helps with alerting
-                "resource_id": resource_id,
-                "resource_metadata": analysis.metadata,
-                "error_type": e.__class__.__name__,  # e.g., "ValueError", "ConnectionError"
-                "error_details": str(e),
-                "traceback": traceback.format_exc(),
-            }
-        )
-        raise HTTPException(status_code=500, detail="Internal server error")
-
-    if analysis.status is None:
-        raise HTTPException(status_code=404, detail="Analysis not found")
-
-    message = ""
-
-    if analysis.status == AnalysisStatus.pending:
-        response.headers["Retry-After"] = "1"
-        message = "Resource is still processing, follow Retry-After header."
-
-    if analysis.status == AnalysisStatus.saved:
-        message = "Analysis completed successfully."
-
-    if analysis.status == AnalysisStatus.failed:
-        message = "Analysis failed. Result is not available."
-
-    return _tree_cover_loss_analytics_response(analysis, message)
-
-
-def _tree_cover_loss_analytics_response(
-    analysis: Analysis, message: str
-) -> TreeCoverLossAnalyticsResponse:
-    return TreeCoverLossAnalyticsResponse(
-        data=TreeCoverLossAnalytics(
-            status=analysis.status,
-            message=message,
-            result=analysis.result,
-            metadata=analysis.metadata,
-        ),
-        status="success",
+    return await get_analysis(
+        resource_id=resource_id,
+        analysis_repository=analysis_repository,
+        response=response,
     )
