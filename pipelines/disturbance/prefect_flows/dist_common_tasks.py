44f84462122bae02e08a6f986b9ba6f42b77f208
Maanas Nukala <manukala6@protonmail.com>
2025-08-14 20:55:09 -0400

    Grasslands analysis (#59)
    * PZB-253 Grasslands parquet and prefect flow
    
    * Add in grassland-specific processing, fix typo, fix graslands_tasks.py filename
    
    Because grasslands does so much processing on the data arrays, I changed
    the early stages (for all analyses) to pass around DataArrays (which is
    pulled out via '.band_data') rather than Datasets.
    
    * Change to new correct grasslands zarr, and Solomon's comments.
    * Revert notebook changes.
    * Make region and subregion groups start at zero
    
    Co-authored-by: Dan Scales <dan.scales@gmail.com>

--- pipelines/disturbance/prefect_flows/dist_common_tasks.py
+++ pipelines/disturbance/prefect_flows/dist_common_tasks.py
@@ -8,12 +8,12 @@ from pipelines.disturbance import stages
 
 @task
 def setup_compute(
-    datasets: Tuple[xr.Dataset, ...],
+    datasets: Tuple[xr.DataArray, ...],
     expected_groups,
     contextual_name: Optional[str] = None,
 ) -> Tuple:
     return stages.setup_compute(datasets, expected_groups, contextual_name)
 
 @task
-def postprocess_result(result: xr.Dataset):
+def postprocess_result(result: xr.DataArray):
     return stages.create_result_dataframe(result)
--- pipelines/disturbance/stages.py
+++ pipelines/disturbance/stages.py
@@ -15,7 +15,7 @@ alerts_confidence = {
 
 
 def setup_compute(
-    datasets: Tuple[xr.Dataset, ...],
+    datasets: Tuple[xr.DataArray, ...],
     expected_groups: Optional[ExpectedGroupsType],
     contextual_column_name: Optional[str] = None,
 ) -> Tuple:
@@ -23,24 +23,24 @@ def setup_compute(
     dist_alerts, country, region, subregion, contextual_layer = datasets
 
     mask = dist_alerts.confidence
-    groupbys: Tuple[xr.Dataset, ...] = (
-        country.band_data.rename("country"),
-        region.band_data.rename("region"),
-        subregion.band_data.rename("subregion"),
+    groupbys: Tuple[xr.DataArray, ...] = (
+        country.rename("country"),
+        region.rename("region"),
+        subregion.rename("subregion"),
         dist_alerts.alert_date,
         dist_alerts.confidence,
     )
     if contextual_layer is not None:
         groupbys = (
             groupbys[:3]
-            + (contextual_layer.band_data.rename(contextual_column_name),)
+            + (contextual_layer.rename(contextual_column_name),)
             + groupbys[3:]
         )
 
     return (mask, groupbys, expected_groups)
 
 
-def create_result_dataframe(alerts_count: xr.Dataset) -> pd.DataFrame:
+def create_result_dataframe(alerts_count: xr.DataArray) -> pd.DataFrame:
     df = common_create_result_dataframe(alerts_count)
     df.rename(columns={'value': 'count'}, inplace=True)
     df.rename(columns={'confidence': 'alert_confidence'}, inplace=True)
