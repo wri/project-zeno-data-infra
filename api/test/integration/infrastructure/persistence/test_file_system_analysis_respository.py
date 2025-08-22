1ae4fd919e94cbaab31ea442e6168ed088e646ce
Gary Tempus <gtempus@users.noreply.github.com>
2025-08-22 00:18:53 -0400

    :bug: fix(FileSystemAnalysisRepository): save status to disk (#76)
    * :bug: fix(FileSystemAnalysisRepository): save status to disk
    
    * :bug: fix(AnalysisService): analysis status is set to `saved` when successful

--- api/test/integration/infrastructure/persistence/test_file_system_analysis_respository.py
+++ api/test/integration/infrastructure/persistence/test_file_system_analysis_respository.py
@@ -1,12 +1,16 @@
-import pytest
-import uuid
 import shutil
-from app.infrastructure.persistence.file_system_analysis_repository import FileSystemAnalysisRepository
-from app.models.common.analysis import AnalysisStatus
-from app.domain.models.analysis import Analysis
+import uuid
 
-TEST_DIRECTORY = 'integration_tests'
-DUMMY_UUID = uuid.UUID('c9787f41-b194-4589-ae53-f45ef290ce6f')
+import pytest
+from app.domain.models.analysis import Analysis
+from app.infrastructure.persistence.file_system_analysis_repository import (
+    FileSystemAnalysisRepository,
+)
+from app.models.common.analysis import AnalysisStatus
+
+TEST_DIRECTORY = "integration_tests"
+DUMMY_UUID = uuid.UUID("c9787f41-b194-4589-ae53-f45ef290ce6f")
+
 
 class TestLoadingAnalysis:
     @pytest.fixture(autouse=True)
@@ -18,7 +22,6 @@ class TestLoadingAnalysis:
         except FileNotFoundError:
             pass  # Ignore if directory doesn't exist
         yield  # Test runs here
-
 
     @pytest.mark.asyncio
     async def test_analysis_is_empty_if_doesnt_exist(self):
@@ -33,17 +36,37 @@ class TestLoadingAnalysis:
             DUMMY_UUID,
             Analysis(
                 result=["data1", "data2"],
-                metadata={ "val1": 12, "val2": "test", "val3": { "key": "value"}},
-                status=AnalysisStatus.saved
-            )
+                metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+                status=AnalysisStatus.saved,
+            ),
         )
 
         analysis_result = await analysis_repository.load_analysis(DUMMY_UUID)
 
         assert analysis_result == Analysis(
             result=["data1", "data2"],
-            metadata={ "val1": 12, "val2": "test", "val3": { "key": "value"}},
-            status=AnalysisStatus.saved
+            metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+            status=AnalysisStatus.saved,
+        )
+
+    @pytest.mark.asyncio
+    async def test_store_failed_analysis_persists_failed_status(self):
+        analysis_repository = FileSystemAnalysisRepository(TEST_DIRECTORY)
+        await analysis_repository.store_analysis(
+            DUMMY_UUID,
+            Analysis(
+                result=["data1", "data2"],
+                metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+                status=AnalysisStatus.failed,
+            ),
+        )
+
+        analysis_result = await analysis_repository.load_analysis(DUMMY_UUID)
+
+        assert analysis_result == Analysis(
+            result=["data1", "data2"],
+            metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+            status=AnalysisStatus.failed,
         )
 
     @pytest.mark.asyncio
@@ -53,25 +76,96 @@ class TestLoadingAnalysis:
             DUMMY_UUID,
             Analysis(
                 result=["data1", "data2"],
-                metadata={ "val1": 12, "val2": "test", "val3": { "key": "value"}},
-                status=AnalysisStatus.saved
-            )
+                metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+                status=AnalysisStatus.saved,
+            ),
         )
 
         await analysis_repository.store_analysis(
             DUMMY_UUID,
             Analysis(
                 result=["data1", "data2"],
-                metadata={ "val1": 12, "val2": "test", "val3": { "key": "value"}},
-                status=AnalysisStatus.saved
-            )
+                metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+                status=AnalysisStatus.saved,
+            ),
         )
 
         analysis_result = await analysis_repository.load_analysis(DUMMY_UUID)
 
         assert analysis_result == Analysis(
             result=["data1", "data2"],
-            metadata={ "val1": 12, "val2": "test", "val3": { "key": "value"}},
-            status=AnalysisStatus.saved
+            metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+            status=AnalysisStatus.saved,
         )
 
+    @pytest.mark.asyncio
+    async def test_update_status_from_pending_to_saved(self):
+        analysis_repository = FileSystemAnalysisRepository(TEST_DIRECTORY)
+        await analysis_repository.store_analysis(
+            DUMMY_UUID,
+            Analysis(
+                result=None,
+                metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+                status=AnalysisStatus.pending,
+            ),
+        )
+
+        analysis_result = await analysis_repository.load_analysis(DUMMY_UUID)
+        assert analysis_result == Analysis(
+            result=None,
+            metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+            status=AnalysisStatus.pending,
+        )
+
+        await analysis_repository.store_analysis(
+            DUMMY_UUID,
+            Analysis(
+                result=["data1", "data2"],
+                metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+                status=AnalysisStatus.saved,
+            ),
+        )
+
+        analysis_result = await analysis_repository.load_analysis(DUMMY_UUID)
+
+        assert analysis_result == Analysis(
+            result=["data1", "data2"],
+            metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+            status=AnalysisStatus.saved,
+        )
+
+    @pytest.mark.asyncio
+    async def test_update_status_from_pending_to_failed(self):
+        analysis_repository = FileSystemAnalysisRepository(TEST_DIRECTORY)
+        await analysis_repository.store_analysis(
+            DUMMY_UUID,
+            Analysis(
+                result=None,
+                metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+                status=AnalysisStatus.pending,
+            ),
+        )
+
+        analysis_result = await analysis_repository.load_analysis(DUMMY_UUID)
+        assert analysis_result == Analysis(
+            result=None,
+            metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+            status=AnalysisStatus.pending,
+        )
+
+        await analysis_repository.store_analysis(
+            DUMMY_UUID,
+            Analysis(
+                result=None,
+                metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+                status=AnalysisStatus.failed,
+            ),
+        )
+
+        analysis_result = await analysis_repository.load_analysis(DUMMY_UUID)
+
+        assert analysis_result == Analysis(
+            result=None,
+            metadata={"val1": 12, "val2": "test", "val3": {"key": "value"}},
+            status=AnalysisStatus.failed,
+        )
