60ed0093dd7f89eb299322dfca4a1023355d3022
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-08-26 17:30:30 -0400

    More manual linting

--- api/app/routers/land_change/tree_cover/tree_cover.py
+++ api/app/routers/land_change/tree_cover/tree_cover.py
@@ -1,3 +1,8 @@
+from fastapi import APIRouter, BackgroundTasks, Depends, Request
+from fastapi import Response as FastAPIResponse
+from fastapi.responses import ORJSONResponse
+from pydantic import UUID5
+
 from app.domain.analyzers.dummy_tree_cover_analyzer import (
     DummyTreeCoverAnalyzer,
 )
@@ -14,10 +19,7 @@ from app.models.land_change.tree_cover import (
 )
 from app.routers.common_analytics import create_analysis, get_analysis
 from app.use_cases.analysis.analysis_service import AnalysisService
-from fastapi import APIRouter, BackgroundTasks, Depends, Request
-from fastapi import Response as FastAPIResponse
-from fastapi.responses import ORJSONResponse
-from pydantic import UUID5
+
 
 ANALYTICS_NAME = "tree_cover"
 router = APIRouter(prefix=f"/{ANALYTICS_NAME}")
