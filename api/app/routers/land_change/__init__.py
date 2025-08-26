d45dab72f9bdc21aad9ca8e03cb46ed951906571
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-08-26 17:17:28 -0400

    Add tree cover to routers (forgot before)

--- api/app/routers/land_change/__init__.py
+++ api/app/routers/land_change/__init__.py
@@ -4,12 +4,14 @@ from .dist_alerts import dist_alerts
 from .land_cover import land_cover_change, land_cover_composition
 from .grasslands import grasslands
 from .natural_lands import natural_lands
+from .tree_cover import tree_cover
 from .tree_cover_gain import tree_cover_gain
 from .tree_cover_loss import tree_cover_loss
 
 router = APIRouter(prefix="/v0/land_change", tags=["β Land Change"])
 router.include_router(dist_alerts.router)
 router.include_router(grasslands.router)
+router.include_router(tree_cover.router)
 router.include_router(tree_cover_loss.router)
 router.include_router(tree_cover_gain.router)
 router.include_router(land_cover_change.router)
