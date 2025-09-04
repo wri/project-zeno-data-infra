d8f5253c13fd513ede900afa699bc34c9a3ee134
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-09-04 14:53:50 -0400

    Implement forest filter for tree cover

--- api/app/domain/analyzers/tree_cover_analyzer.py
+++ api/app/domain/analyzers/tree_cover_analyzer.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from app.domain.analyzers.analyzer import Analyzer
 from app.domain.compute_engines.compute_engine import ComputeEngine
 from app.domain.models.analysis import Analysis
@@ -11,17 +13,37 @@ class TreeCoverAnalyzer(Analyzer):
 
     async def analyze(self, analysis: Analysis):
         tree_cover_analytics_in = TreeCoverAnalyticsIn(**analysis.metadata)
+        groupbys = []
+
+        filters: List[DatasetFilter] = [
+            DatasetFilter(
+                dataset=Dataset.canopy_cover,
+                op=">=",
+                value=tree_cover_analytics_in.canopy_cover,
+            ),
+        ]
+
+        if tree_cover_analytics_in.forest_filter is not None:
+            forest_filter = Dataset.primary_forest
+            if tree_cover_analytics_in.forest_filter == Dataset.intact_forest.value:
+                forest_filter = Dataset.intact_forest
+
+            filters.append(
+                DatasetFilter(
+                    dataset=forest_filter,
+                    op="=",
+                    value=True,
+                )
+            )
+
+        # if tree_cover_analytics_in.intersections is not None:
+        #     for intersection in tree_cover_analytics_in.intersections:
+        #         groupbys.append(
 
         query = DatasetQuery(
             aggregate=DatasetAggregate(dataset=Dataset.area_hectares, func="sum"),
-            group_bys=[],
-            filters=[
-                DatasetFilter(
-                    dataset=Dataset.canopy_cover,
-                    op=">=",
-                    value=tree_cover_analytics_in.canopy_cover,
-                ),
-            ],
+            group_bys=groupbys,
+            filters=filters
         )
         return await self.compute_engine.compute(
             tree_cover_analytics_in.aoi.type,
