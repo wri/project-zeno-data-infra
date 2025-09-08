e77e6f99995a5071d118900e37b6d5d6b5270dce
Gary Tempus <gtempus@users.noreply.github.com>
2025-09-08 13:10:20 -0400

    feat(TreeCoverGainAnalyzer): add multiple year ranges and forest filter (#98)

--- api/app/domain/repositories/zarr_dataset_repository.py
+++ api/app/domain/repositories/zarr_dataset_repository.py
@@ -1,3 +1,5 @@
+import ast
+
 import xarray as xr
 from app.domain.models.dataset import Dataset
 from shapely import Geometry
@@ -8,7 +10,10 @@ class ZarrDatasetRepository:
     ZARR_LOCATIONS = {
         Dataset.area_hectares: "s3://gfw-data-lake/umd_area_2013/v1.10/raster/epsg-4326/zarr/pixel_area_ha.zarr",
         Dataset.tree_cover_loss: "s3://gfw-data-lake/umd_tree_cover_loss/v1.12/raster/epsg-4326/zarr/year.zarr",
+        Dataset.tree_cover_gain: "s3://gfw-data-lake/umd_tree_cover_gain_from_height/v20240126/raster/epsg-4326/zarr/period.zarr",
         Dataset.canopy_cover: "s3://gfw-data-lake/umd_tree_cover_density_2000/v1.8/raster/epsg-4326/zarr/threshold.zarr",
+        Dataset.primary_forest: "s3://gfw-data-lake/umd_regional_primary_forest_2001/v201901/raster/epsg-4326/zarr/is.zarr",
+        Dataset.intact_forest: "s3://gfw-data-lake/ifl_intact_forest_landscapes_2000/v2021/raster/epsg-4326/zarr/is.zarr",
     }
 
     def load(self, dataset: Dataset, geometry: Geometry = None) -> xr.DataArray:
@@ -47,6 +52,11 @@ class ZarrDatasetRepository:
                     return 7
         elif dataset == Dataset.tree_cover_loss:
             return int(value) - 2000
+        elif dataset == Dataset.tree_cover_gain:
+            value_tuple = ast.literal_eval(value)
+            val_map = {"2000-2005": 1, "2005-2010": 2, "2010-2015": 3, "2015-2020": 4}
+
+            return [val_map[val] for val in value_tuple]
         else:
             raise NotImplementedError()
 
@@ -56,6 +66,23 @@ class ZarrDatasetRepository:
         """
         if dataset == Dataset.tree_cover_loss:
             return series + 2000
+        elif dataset == Dataset.tree_cover_gain:
+
+            def pixel_to_gain(val):
+                match val:
+                    case 0:
+                        return ""
+                    case 1:
+                        return "2000-2005"
+                    case 2:
+                        return "2005-2010"
+                    case 3:
+                        return "2010-2015"
+                    case 4:
+                        return "2015-2020"
+
+            return series.map(pixel_to_gain)
+
         else:
             return series
 
