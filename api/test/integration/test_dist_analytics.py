491ef81fc395ac7004449abafdbad4a5b6d99519
Gary Tempus Jr <gary.tempus@wri.org>
2025-09-11 15:39:21 -0400

    :white_check_mark: test(DistAlerts): partial integration test update

--- api/test/integration/test_dist_analytics.py
+++ api/test/integration/test_dist_analytics.py
@@ -14,7 +14,10 @@ from app.infrastructure.persistence.file_system_analysis_repository import (
     FileSystemAnalysisRepository,
 )
 from app.main import app
-from app.models.common.areas_of_interest import AdminAreaOfInterest
+from app.models.common.areas_of_interest import (
+    AdminAreaOfInterest,
+    KeyBiodiversityAreaOfInterest,
+)
 from app.models.land_change.dist_alerts import DistAlertsAnalyticsIn
 from app.routers.land_change.dist_alerts.dist_alerts import (
     ANALYTICS_NAME,
@@ -444,55 +447,64 @@ class TestDistAnalyticsPostWithMultipleAdminAOIs:
 
 
 class TestDistAnalyticsPostWithMultipleKBAAOIs:
-    @pytest_asyncio.fixture(autouse=True)
+    @pytest_asyncio.fixture
     async def setup(self):
         """Runs before each test in this class"""
-        delete_resource_files("dist_alerts", "31429ede-d718-5723-8061-edcc2677073e")
+        analytics_in = DistAlertsAnalyticsIn(
+            aoi=KeyBiodiversityAreaOfInterest(
+                type="key_biodiversity_area",
+                ids=["18392", "46942", "18407"],
+            ),
+            start_date="2025-02-01",
+            end_date="2025-04-30",
+            intersections=[],
+        )
+        app.dependency_overrides[
+            create_analysis_service
+        ] = create_analysis_service_for_tests
+        app.dependency_overrides[
+            get_analysis_repository
+        ] = get_file_system_analysis_repository
+
+        delete_resource_files(ANALYTICS_NAME, analytics_in.thumbprint())
 
         async with LifespanManager(app):
             async with AsyncClient(
                 transport=ASGITransport(app), base_url="http://testserver"
             ) as client:
                 request = await client.post(
-                    "/v0/land_change/dist_alerts/analytics",
-                    json={
-                        "aoi": {
-                            "type": "key_biodiversity_area",
-                            "ids": ["18392", "46942", "18407"],
-                        },
-                        "start_date": "2025-02-01",
-                        "end_date": "2025-04-30",
-                        "intersections": [],
-                    },
+                    f"/v0/land_change/{ANALYTICS_NAME}/analytics",
+                    json=analytics_in.model_dump(),
                 )
 
-                yield (request, client)
+                yield request, client, analytics_in
 
     @pytest.mark.asyncio
     async def test_post_returns_pending_status(self, setup):
-        test_request, _ = setup
+        test_request, _, _ = setup
         resource = test_request.json()
         assert resource["status"] == "pending"
 
     @pytest.mark.asyncio
     async def test_post_returns_resource_link(self, setup):
-        test_request, _ = setup
+        test_request, _, analysis_params = setup
         resource = test_request.json()
         assert (
             resource["data"]["link"]
-            == "http://testserver/v0/land_change/dist_alerts/analytics/31429ede-d718-5723-8061-edcc2677073e"
+            == f"http://testserver/v0/land_change/{ANALYTICS_NAME}/analytics/{analysis_params.thumbprint()}"
         )
 
     @pytest.mark.asyncio
     async def test_post_returns_202_accepted_response_code(self, setup):
-        test_request, _ = setup
+        test_request, _, _ = setup
         assert test_request.status_code == 202
 
     @pytest.mark.asyncio
     async def test_resource_calculate_results(self, setup):
-        test_request, client = setup
-        resource_id = test_request.json()["data"]["link"].split("/")[-1]
-        data = await retry_getting_resource("dist_alerts", resource_id, client)
+        test_request, client, analysis_params = setup
+        data = await retry_getting_resource(
+            ANALYTICS_NAME, analysis_params.thumbprint(), client
+        )
         result = pd.DataFrame(data["result"])
 
         # 1. Validate expected columns
