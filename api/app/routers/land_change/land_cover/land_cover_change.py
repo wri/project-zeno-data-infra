c656a6720b320a481a0d2755e4793532eb3646dc
Gary Tempus Jr <gary.tempus@wri.org>
2025-09-11 18:24:29 -0400

    feat(LandCoverChange): use `AwsDynamoDbS3AnalysisRepository`

--- api/app/routers/land_change/land_cover/land_cover_change.py
+++ api/app/routers/land_change/land_cover/land_cover_change.py
@@ -1,7 +1,7 @@
 from app.domain.analyzers.land_cover_change_analyzer import LandCoverChangeAnalyzer
 from app.domain.repositories.analysis_repository import AnalysisRepository
-from app.infrastructure.persistence.file_system_analysis_repository import (
-    FileSystemAnalysisRepository,
+from app.infrastructure.persistence.aws_dynamodb_s3_analysis_repository import (
+    AwsDynamoDbS3AnalysisRepository,
 )
 from app.models.common.analysis import AnalyticsOut
 from app.models.common.base import DataMartResourceLinkResponse
@@ -22,7 +22,7 @@ router = APIRouter(prefix=f"/{ANALYTICS_NAME}")
 
 
 def get_analysis_repository() -> AnalysisRepository:
-    return FileSystemAnalysisRepository(ANALYTICS_NAME)
+    return AwsDynamoDbS3AnalysisRepository(ANALYTICS_NAME)
 
 
 def create_analysis_service(
