d80e00f0615200fde4d1f8c5b0cd6c0cf1e06bcb
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-09-13 21:24:10 -0400

    Raise exception on auth failure, in case this is evr used.

--- api/app/routers/common_auth.py
+++ api/app/routers/common_auth.py
@@ -2,7 +2,8 @@ import logging
 import os
 from typing import Annotated
 
-from fastapi import Header
+from fastapi import Header, HTTPException
+from starlette import status
 
 
 async def get_secret():
@@ -14,16 +15,19 @@ async def verify_shared_secret(
     x_client_secret: Annotated[str | None, Header(alias="X-Client-Secret")] = None,
 ):
     secret = await get_secret()
-    if not x_client_secret or x_client_secret != secret:
-        logging.warning(
-            {"event": "shared_secret_verification_failure", "user": "some stranger"}
-        )
-        # Once the front-end is ready, short-circuit the request with 401
-        # raise HTTPException(
-        #     status_code=status.HTTP_401_UNAUTHORIZED,
-        #     detail="Invalid or missing shared secret",
-        # )
-    else:
+    if x_client_secret and x_client_secret == secret:
         logging.info(
             {"event": "shared_secret_verification_success", "user": "some friend"}
+        )
+    else:
+        logging.warning(
+            {
+                "event": "shared_secret_verification_failure",
+                "user": "some stranger",
+                "severity": "medium",
+            }
+        )
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or missing shared secret",
         )
