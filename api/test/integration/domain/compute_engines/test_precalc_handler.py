7d5ac85c1ea1f6263a233d19064f154e72dffc9a
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-09-19 15:37:41 -0400

    Fix key error for some params in tree cover gain

--- api/test/integration/domain/compute_engines/test_precalc_handler.py
+++ api/test/integration/domain/compute_engines/test_precalc_handler.py
@@ -20,7 +20,7 @@ from app.infrastructure.external_services.duck_db_query_service import (
 
 class TestTreeCoverGainPrecalcHandler:
     @pytest.mark.asyncio
-    async def test_happy_path(self):
+    async def test_sql_string_for_one_period(self):
         query_service = MagicMock(spec=DuckDbPrecalcQueryService)
         handler = TreeCoverGainPrecalcHandler(
             precalc_query_builder=PrecalcSqlQueryBuilder(),
@@ -35,7 +35,7 @@ class TestTreeCoverGainPrecalcHandler:
                 DatasetFilter(
                     dataset=Dataset.tree_cover_gain,
                     op="in",
-                    value="('2000-2005')",
+                    value=("2000-2005",),
                 ),
             ],
         )
@@ -45,5 +45,34 @@ class TestTreeCoverGainPrecalcHandler:
         await handler.handle(aoi_type, ["AUS"], query)
 
         query_service.execute.assert_called_once_with(
-            "SELECT aoi_id, aoi_type, SUM(area_ha) AS area_ha FROM data_source WHERE tree_cover_gain_period in ('2000-2005') AND aoi_id in ('AUS') GROUP BY aoi_id, aoi_type",
+            "SELECT aoi_id, aoi_type, SUM(area_ha) AS area_ha FROM data_source WHERE tree_cover_gain_period in ('2000-2005') AND aoi_id in ('AUS') GROUP BY aoi_id, aoi_type"
+        )
+
+    @pytest.mark.asyncio
+    async def test_sql_string_for_multiple_periods(self):
+        query_service = MagicMock(spec=DuckDbPrecalcQueryService)
+        handler = TreeCoverGainPrecalcHandler(
+            precalc_query_builder=PrecalcSqlQueryBuilder(),
+            precalc_query_service=query_service,
+            next_handler=None,
+        )
+
+        query = DatasetQuery(
+            aggregate=DatasetAggregate(datasets=[Dataset.area_hectares], func="sum"),
+            group_bys=[],
+            filters=[
+                DatasetFilter(
+                    dataset=Dataset.tree_cover_gain,
+                    op="in",
+                    value=("2000-2005", "2005-2010"),
+                ),
+            ],
+        )
+
+        aoi_type = "admin"
+
+        await handler.handle(aoi_type, ["AUS"], query)
+
+        query_service.execute.assert_called_once_with(
+            "SELECT aoi_id, aoi_type, SUM(area_ha) AS area_ha FROM data_source WHERE tree_cover_gain_period in ('2000-2005', '2005-2010') AND aoi_id in ('AUS') GROUP BY aoi_id, aoi_type"
         )
