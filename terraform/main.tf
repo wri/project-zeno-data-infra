3cefa8101074da7c86316d59ab34ba2ba25ada27
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-09-22 18:21:42 -0400

    Try specifying to create API logs (#150)
    Try creating API logs

--- terraform/main.tf
+++ terraform/main.tf
@@ -246,9 +246,8 @@ module "analytics" {
           value = var.new_relic_license_key
         }
       ]
-      create_cloudwatch_log_group = false
+      create_cloudwatch_log_group = true
       enable_cloudwatch_logging = true
-
     }
   }
 
