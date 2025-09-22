fce38714113eaff36a8dd34b8500b2dd749a36cd
Solomon Negusse <solomon.negusse@wri.org>
2025-09-22 10:10:34 +0300

    revert state key change

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-state-zeno-rest-api"
-    # key            = "terraform/state/production/terraform.tfstate"
+    key            = "terraform/state/production/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-locks-production"
