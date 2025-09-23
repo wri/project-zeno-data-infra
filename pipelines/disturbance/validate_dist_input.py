5088c96a0f258a30ede4011f7f2dfef9a520a6e2
Dan Scales <dan.scales@gmail.com>
2025-09-23 13:24:01 -0700

    Fix broken validate_zonal_statistics.
    In Justin's final rename of columns, missed renaming 'confidence' ->
    'dist_alerts_confidence' in a few places. Also, important import was
    removed, not sure if that was done by the reformatter?
    
    Sorry, 'black' decided to reformat some other code.

--- pipelines/disturbance/validate_dist_input.py
+++ pipelines/disturbance/validate_dist_input.py
@@ -1,8 +1,8 @@
-import xarray as xr
 import numpy as np
-
+import xarray as xr
 
 DIST_COUNT_THRESHOLD = 5
+
 
 def validates_dist_input(curr_cog_uri: str, prev_cog_uri: str) -> None:
     """
@@ -14,8 +14,11 @@ def validates_dist_input(curr_cog_uri: str, prev_cog_uri: str) -> None:
     prev_dist_alerts = _read_from_cog(prev_cog_uri)
 
     assert _check_percent_diff(curr_dist_alerts, prev_dist_alerts, DIST_COUNT_THRESHOLD)
-    
-def _check_percent_diff(da1: xr.DataArray, da2: xr.DataArray, diff_threshold: float) -> bool:
+
+
+def _check_percent_diff(
+    da1: xr.DataArray, da2: xr.DataArray, diff_threshold: float
+) -> bool:
     stat1 = da1.count()
     stat2 = da2.count()
 
@@ -25,14 +28,13 @@ def _check_percent_diff(da1: xr.DataArray, da2: xr.DataArray, diff_threshold: fl
 
 def _read_from_cog(cog_uri: str):
     """
-    Read COG to an xarray. Using chunking the aligns with COG internal tiling for faster stats, since 
+    Read COG to an xarray. Using chunking the aligns with COG internal tiling for faster stats, since
     this read isn't being used for converting to zarr.
     """
-    dist_alerts = xr.open_dataset(
-        cog_uri,
-        chunks={'x': 8192, 'y':8192}
-    ).astype(np.int16).band_data
+    dist_alerts = (
+        xr.open_dataset(cog_uri, chunks={"x": 8192, "y": 8192})
+        .astype(np.int16)
+        .band_data
+    )
 
     return dist_alerts
-
-
