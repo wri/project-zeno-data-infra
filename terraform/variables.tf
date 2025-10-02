07324d47feed2c348141e4c96c6c2d8286d55c32
Solomon Negusse <solomon.negusse@wri.org>
2025-10-02 17:31:34 +0300

    run data updates on ecs and create a dist prefect deployment to run on daily schedule

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -32,3 +32,66 @@ variable "new_relic_license_key" {
   type        = string
   description = "New Relic License Key"
 }
+
+variable "prefect_api_key" {
+  description = "Prefect Cloud API key"
+  type        = string
+  sensitive   = true
+}
+
+variable "prefect_account_id" {
+  description = "Prefect Cloud account ID"
+  type        = string
+}
+
+variable "prefect_workspace_id" {
+  description = "Prefect Cloud workspace ID"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Global Nature Watch Analytics Pipeline"
+  type        = string
+  default     = "GNW-Analytics-Pipeline"
+}
+
+variable "work_pool_name" {
+  description = "Name of the Prefect work pool"
+  type        = string
+  default     = "gnw-pipeline-ecs-work-pool"
+}
+
+variable "worker_count" {
+  description = "Number of Prefect workers to run"
+  type        = number
+  default     = 2
+}
+
+variable "worker_cpu" {
+  description = "CPU units for worker tasks"
+  type        = number
+  default     = 2048 # 2 vCPU
+}
+
+variable "worker_memory" {
+  description = "Memory for worker tasks in MB"
+  type        = number
+  default     = 8192 # 8 GB
+}
+
+variable "flow_cpu" {
+  description = "Default CPU units for flow run tasks"
+  type        = number
+  default     = 2048 # 2 vCPU
+}
+
+variable "flow_memory" {
+  description = "Default memory for flow run tasks in MB"
+  type        = number
+  default     = 4096 # 4 GB
+}
+
+variable "coiled_token" {
+  description = "Access token to write dask on Coiled"
+  type = string
+}
