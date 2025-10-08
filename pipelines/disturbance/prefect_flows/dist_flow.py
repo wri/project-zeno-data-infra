ab6508d11a3857689635e07ae81979391290d148
Solomon Negusse <solomon.negusse@wri.org>
2025-10-08 17:33:32 +0300

    clean up prefect flows fixing naming and organization of flows

--- pipelines/disturbance/prefect_flows/dist_flow.py
+++ pipelines/disturbance/prefect_flows/dist_flow.py
@@ -1,16 +1,11 @@
 import logging
-import os
 
-import coiled
 from prefect import flow, task
 from prefect.logging import get_run_logger
 
-from pipelines.carbon_flux.prefect_flows import carbon_flow
 from pipelines.disturbance import prefect_flows, validate_zonal_statistics
 from pipelines.disturbance.check_for_new_alerts import get_latest_version
 from pipelines.disturbance.create_zarr import create_zarr as create_zarr_func
-from pipelines.grasslands.prefect_flows import grasslands_flow
-from pipelines.natural_lands.prefect_flows import nl_flow as nl_prefect_flow
 
 logging.getLogger("distributed.client").setLevel(logging.ERROR)
 
@@ -18,25 +13,6 @@ logging.getLogger("distributed.client").setLevel(logging.ERROR)
 @task
 def get_new_dist_version() -> str:
     return get_latest_version("umd_glad_dist_alerts")
-
-
-@task
-def create_cluster():
-    cluster = coiled.Cluster(
-        name="dist_alerts_zonal_stat_count",
-        region="us-east-1",
-        n_workers=10,
-        tags={"project": "dist_alerts_zonal_stat"},
-        scheduler_vm_types=["r7g.xlarge"],
-        worker_vm_types=["r7g.2xlarge"],
-        compute_purchase_option="spot_with_fallback",
-        no_client_timeout="5 seconds",
-        container=os.getenv("PIPELINES_IMAGE"),
-    )
-    cluster.adapt(minimum=10, maximum=50)
-
-    client = cluster.get_client()
-    return client, cluster
 
 
 @task
@@ -50,7 +26,7 @@ def run_validation_suite(gadm_dist_result):
     validate_zonal_statistics.validate(gadm_dist_result)
 
 
-@flow(name="dist_alerts_flow", log_prints=True)
+@flow(name="DIST alerts", log_prints=True)
 def dist_alerts_flow(overwrite=False) -> list[str]:
     logger = get_run_logger()
     dask_client = None
@@ -58,13 +34,6 @@ def dist_alerts_flow(overwrite=False) -> list[str]:
     try:
         dist_version = get_new_dist_version()
         logger.info(f"Latest dist version: {dist_version}")
-        dask_client, _ = create_cluster()
-
-        gl_result = grasslands_flow.gadm_grasslands_area(overwrite=overwrite)
-        result_uris.append(gl_result)
-
-        nl_result = nl_prefect_flow.gadm_natural_lands_area(overwrite=overwrite)
-        result_uris.append(nl_result)
 
         dist_zarr_uri = create_zarr(dist_version, overwrite=overwrite)
         gadm_dist_result = prefect_flows.dist_alerts_area(
@@ -94,10 +63,7 @@ def dist_alerts_flow(overwrite=False) -> list[str]:
         )
         result_uris.append(gadm_dist_by_land_cover_result)
 
-        validate_result = run_validation_suite(gadm_dist_result)
-
-        carbon_result = carbon_flow.gadm_carbon_flux(overwrite=overwrite)
-        result_uris.append(carbon_result)
+        run_validation_suite(gadm_dist_result)
 
     except Exception:
         logger.error("DIST alerts analysis failed.")
