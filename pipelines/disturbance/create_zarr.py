d060923cbc98ef8badcfcb35a3f8f37a9859e304
Justin Terry <justin.terry@wri.org>
2025-10-09 10:15:49 -0700

    Pass version properly in pipelines

--- pipelines/disturbance/create_zarr.py
+++ pipelines/disturbance/create_zarr.py
@@ -1,5 +1,6 @@
 import numpy as np
 import xarray as xr
+
 from pipelines.globals import ANALYTICS_BUCKET, DATA_LAKE_BUCKET
 from pipelines.utils import s3_uri_exists
 
@@ -16,15 +17,17 @@ def decode_alert_data(band_data) -> xr.Dataset:
 def create_zarr(version, overwrite=False) -> str:
     """create a full extent zarr file in s3."""
     base_folder = f"umd_glad_dist_alerts/{version}/raster/epsg-4326"
-    zarr_uri = f"s3://{ANALYTICS_BUCKET}/zarr/umd_glad_dist_alerts.zarr"
+    zarr_uri = (
+        f"s3://{ANALYTICS_BUCKET}/zarr/dist-alerts/{version}/umd_glad_dist_alerts.zarr"
+    )
     cog_uri = f"s3://{DATA_LAKE_BUCKET}/{base_folder}/cog/default.tif"
 
     if s3_uri_exists(f"{zarr_uri}/zarr.json") and not overwrite:
         return zarr_uri
 
-    dataset = xr.open_dataset(cog_uri, chunks="auto").band_data.chunk(
-        {"x": 10000, "y": 10000}
-    )
+    dataset = xr.open_dataset(
+        cog_uri, chunks="auto", storage_options={"requester_pays": True}
+    ).band_data.chunk({"x": 10000, "y": 10000})
 
     decoded_alert_data = decode_alert_data(dataset)
     decoded_alert_data.to_zarr(zarr_uri, mode="w")
