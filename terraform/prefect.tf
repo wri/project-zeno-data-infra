a53c636b1e51dd741543922042c2c42c41e732ef
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-10-17 14:48:43 -0400

    Enable deploying a commit hash to a specific workspace on demand (#171)
    * Add ability to deploy on demand to isolated workspaces
    * Improve isolation of resources across workspaces

--- terraform/prefect.tf
+++ terraform/prefect.tf
@@ -47,7 +47,7 @@ resource "prefect_block" "aws_credentials" {
   data = jsonencode({
     aws_access_key_id     = var.aws_access_key_id
     aws_secret_access_key = var.aws_secret_access_key
-    region_name           = "us-east-1"
+    region_name           = var.region
   })
 }
 
