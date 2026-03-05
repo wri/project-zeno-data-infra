fc957e8692ef50e892fd86237355b0347554bdb6
Solomon Negusse <solomon.negusse@wri.org>
2026-03-05 16:39:08 +0300

    fix tcl results location

--- pipelines/tree_cover_loss/prefect_flows/tcl_flow.py
+++ pipelines/tree_cover_loss/prefect_flows/tcl_flow.py
@@ -29,7 +29,7 @@ def umd_tree_cover_loss_flow(
     Returns:
         The S3 URI for the saved parquet result.
     """
-    result_uri = f"s3://{ANALYTICS_BUCKET}/zonal-statistics/{version}/admin-tree-cover-loss.parquet"
+    result_uri = f"s3://{ANALYTICS_BUCKET}/zonal-statistics/tcl/{version}/admin-tree-cover-loss.parquet"
 
     if not overwrite and s3_uri_exists(result_uri):
         return result_uri
