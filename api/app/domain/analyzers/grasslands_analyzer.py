561224f50d1a987f98b17701ba0e0f3e84423d40
Daniel Mannarino <daniel.mannarino@gmail.com>
2026-04-02 22:19:37 -0400

    Remove dead code copying _input_uris. All unit tests still passing.

--- api/app/domain/analyzers/grasslands_analyzer.py
+++ api/app/domain/analyzers/grasslands_analyzer.py
@@ -35,8 +35,7 @@ class GrasslandsAnalyzer(Analyzer):
     @nr_agent.function_trace(name="GrasslandsAnalyzer.analyze")
     async def analyze(self, analysis: Analysis) -> None:
         grasslands_analytics_in = GrasslandsAnalyticsIn(**analysis.metadata)
-        if analysis.metadata.get("_input_uris") is not None:
-            grasslands_analytics_in._input_uris = analysis.metadata["_input_uris"]
+
         if grasslands_analytics_in.aoi.type == "admin":
             gadm_ids: List = grasslands_analytics_in.aoi.ids
             results: Dict = await self.analyze_admin_areas(
