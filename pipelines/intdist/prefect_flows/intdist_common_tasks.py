d01bda09d6f42adff0740c12002c2a5577ee4032
Dan Scales <dan.scales@gmail.com>
2026-06-06 19:53:35 -0700

    PZB-965 Adding GADM int-dist alerts area to the int-dist flow.
    Had to add in 10m-resolution pixel area and GADM zarrs.
    
    This is an initial draft - may be able to share some more common code with the dist
    alerts pipeline.
    
    Also, didn't try to in any other analyses with contextual layers such as
    grasslands, natural lands, etc., since those are decided on yet.

---
+++ pipelines/intdist/prefect_flows/intdist_common_tasks.py
@@ -0,0 +1,30 @@
+from typing import Optional, Tuple
+
+import xarray as xr
+from prefect import task
+
+from pipelines.intdist import stages
+from pipelines.globals import ANALYTICS_BUCKET
+
+DIST_PREFIX = f"s3://{ANALYTICS_BUCKET}/zonal-statistics/intdist-alerts"
+
+
+@task
+def load_data(
+    zarr_uri: str, contextual_uri: Optional[str] = None
+) -> Tuple[xr.DataArray, ...]:
+    return stages.load_data(zarr_uri, contextual_uri)
+
+
+@task
+def setup_compute(
+    datasets: Tuple[xr.DataArray, ...],
+    expected_groups,
+    contextual_name: Optional[str] = None,
+) -> Tuple:
+    return stages.setup_compute(datasets, expected_groups, contextual_name)
+
+
+@task
+def postprocess_result(result: xr.DataArray):
+    return stages.create_result_dataframe(result)
