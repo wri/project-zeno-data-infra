3faf3667a14d71999aa0fe759d174ac6a074fca0
Daniel Mannarino <daniel.mannarino@gmail.com>
2026-06-11 11:00:16 -0400

    Promote the new pixel area Zarr from staging to prod

--- api/app/domain/analyzers/tree_cover_loss_analyzer.py
+++ api/app/domain/analyzers/tree_cover_loss_analyzer.py
@@ -34,8 +34,7 @@ INPUT_URIS: Dict[Environment, Dict[str, str]] = {
                 Dataset.tree_cover_loss,
                 Dataset.tree_cover_loss_drivers,
             ]
-        },
-        "admin_results_uri": "s3://lcl-analytics/zonal-statistics/tcl/v1.13/admin-tree-cover-loss_v20260609.parquet",
+        }
     },
     Environment.production: {
         **{
@@ -51,7 +50,7 @@ INPUT_URIS: Dict[Environment, Dict[str, str]] = {
             ]
         },
         # Should match result_uri in tcl_flow.py in pipelines.
-        "admin_results_uri": "s3://lcl-analytics/zonal-statistics/tcl/v1.13/admin-tree-cover-loss_v20260518.parquet",
+        "admin_results_uri": "s3://lcl-analytics/zonal-statistics/tcl/v1.13/admin-tree-cover-loss_v20260609.parquet",
     },
 }
 
