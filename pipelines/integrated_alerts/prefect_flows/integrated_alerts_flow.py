145fafad5d1acfbe4e32abb21aac1ea80294e7de
Dan Scales <dan.scales@gmail.com>
2026-07-07 11:22:03 -0700

    Record latest version of int alerts when is_latest flag is set

--- pipelines/integrated_alerts/prefect_flows/integrated_alerts_flow.py
+++ pipelines/integrated_alerts/prefect_flows/integrated_alerts_flow.py
@@ -1,11 +1,13 @@
 import logging
 
+import boto3
 from prefect import flow, task
 from prefect.logging import get_run_logger
 
 from pipelines.integrated_alerts.prefect_flows.gadm_integrated_alerts import integrated_alerts_area
 from pipelines.disturbance.check_for_new_alerts import get_latest_version
 from pipelines.integrated_alerts.create_zarr import create_zarr
+from pipelines.globals import ANALYTICS_BUCKET
 
 logging.getLogger("distributed.client").setLevel(logging.ERROR)
 
@@ -20,17 +22,28 @@ def create_zarr_task(dist_version: str, overwrite=False) -> str:
     zarr_uri = create_zarr(dist_version, overwrite=overwrite)
     return zarr_uri
 
+@task
+def write_int_latest_version(version) -> None:
+    s3_client = boto3.client("s3")
+    s3_client.put_object(
+        Bucket=ANALYTICS_BUCKET,
+        Key="zonal-statistics/integrated-alerts/latest",
+        Body=version.encode("utf-8"),
+        ContentType="text/plain",
+    )
+
 
 @flow(
     name="Create int-Dist zarr",
     log_prints=True,
     description="Create zarr from tiles of one version of the integrated disturbance alerts dataset",
 )
-def integrated_alerts_zarr_flow(version=None, overwrite=False) -> list[str]:
+def integrated_alerts_zarr_flow(version=None, overwrite=False, is_latest=False) -> list[str]:
     logger = get_run_logger()
     result_uris = []
 
     if version is None:
+        is_latest = True
         version = get_new_integrated_alerts_version()
         logger.info(f"Latest int-dist version: {version}")
 
@@ -41,5 +54,8 @@ def integrated_alerts_zarr_flow(version=None, overwrite=False) -> list[str]:
         integrated_alerts_zarr_uri, version, overwrite=overwrite
     )
     result_uris.append(gadm_dist_result)
+
+    if is_latest:
+        write_int_latest_version(version)
 
     return result_uris
